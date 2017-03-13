6c5d9f59db7f703da414f45abb1a447361b2ceba
Lars Kristian Dahl <lars.dahl@gmail.com>
2017-03-13 08:56:40 +0100

    Fix movement system

--- core/src/no/ntnu/tdt4240/asteroids/entity/system/MovementSystem.java
+++ core/src/no/ntnu/tdt4240/asteroids/entity/system/MovementSystem.java
@@ -3,7 +3,6 @@ package no.ntnu.tdt4240.asteroids.entity.system;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
-import com.badlogic.ashley.utils.ImmutableArray;
 
 import no.ntnu.tdt4240.asteroids.entity.component.PositionComponent;
 import no.ntnu.tdt4240.asteroids.entity.component.VelocityComponent;
@@ -16,8 +15,6 @@ public class MovementSystem extends IteratingSystem {
 
     private static final String TAG = MovementSystem.class.getSimpleName();
 
-    private ImmutableArray<Entity> entities;
-
     public MovementSystem() {
         super(Family.all(PositionComponent.class, VelocityComponent.class).get());
     }
@@ -26,7 +23,7 @@ public class MovementSystem extends IteratingSystem {
     protected void processEntity(Entity entity, float deltaTime) {
         PositionComponent position = positionMapper.get(entity);
         VelocityComponent velocity = velocityMapper.get(entity);
-        position.x = velocity.x * deltaTime;
-        position.y = velocity.y * deltaTime;
+        position.setX(position.getX() + velocity.getX() * deltaTime);
+        position.setY(position.getY() + velocity.getY() * deltaTime);
     }
 }
